7d6b73d42821ef26ac75eab778a49276cf1481be
gerry <13632429781@163.com>
2022-02-18 00:32:24 +0800

    feat(api): 配置方案的实现

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module gohub
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.7 // indirect
+require (
+	github.com/gin-gonic/gin v1.7.7
+	github.com/spf13/cast v1.4.1 // indirect
+	github.com/spf13/viper v1.10.1
+)
--- main.go
+++ main.go
@@ -1,20 +1,36 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"gohub/bootstrap"
+	config2 "gohub/config"
+	"gohub/pkg/config"
+
+	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	// 加载 config 目录下的配置信息
+	config2.Initialize()
+}
+
 func main() {
-	// new 一个Gin Engine 实例air
+
+	// 配置初始化
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+
+	config.InitConfig(env)
+
+	// new 一个Gin Engine 实例
 	r := gin.New()
 
 	// 初始化路由绑定
 	bootstrap.SetupRoute(r)
-
 	// 运行服务
-	err := r.Run(":8000")
+	err := r.Run(":" + config.Get("app.port"))
 	if err != nil {
 		fmt.Println(err.Error())
 	}
