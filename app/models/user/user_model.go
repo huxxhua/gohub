d28ef70ff2680425e88522dc2a715a064d83e107
gerry <13632429781@163.com>
2022-02-19 23:37:05 +0800

    feat(api): 模型初始化，新增用户模型

---
+++ app/models/user/user_model.go
@@ -0,0 +1,17 @@
+// Package user 存放用户 Model 相关逻辑
+package user
+
+import "gohub/app/models"
+
+// User 用户模型
+type User struct {
+	models.BaseModel
+
+	Name string `json:"name,omitempty"`
+	// JSON 解析器忽略字段
+	Email    string `json:"-"`
+	Phone    string `json:"-"`
+	Password string `json:"-"`
+
+	models.CommonTimestampsField
+}
