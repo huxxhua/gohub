01b7b46c2a1a9c8ade4a6b9df817afc7c5ad4467
gerry <13632429781@163.com>
2022-03-04 10:33:23 +0800

    feat(api): 发送邮件（mail 包）

---
+++ pkg/mail/driver_smtp.go
@@ -0,0 +1,44 @@
+package mail
+
+import (
+	"fmt"
+	email2 "github.com/jordan-wright/email"
+	"gohub/pkg/logger"
+	"net/smtp"
+)
+
+// SMTP 实现 email.Driver interface
+type SMTP struct{}
+
+// Send 实现 email.Driver interface 的 Send 方法
+func (s *SMTP) Send(email Email, config map[string]string) bool {
+
+	e := email2.NewEmail()
+
+	e.From = fmt.Sprintf("%v <%v>", email.From.Name, email.From.Address)
+	e.To = email.To
+	e.Bcc = email.Bcc
+	e.Cc = email.Cc
+	e.Subject = email.Subject
+	e.Text = email.Text
+	e.HTML = email.HTML
+
+	logger.DebugJSON("发送邮件", "发件详情", e)
+
+	err := e.Send(
+		fmt.Sprintf("%v:%v", config["host"], config["port"]),
+
+		smtp.PlainAuth(
+			"",
+			config["username"],
+			config["password"],
+			config["host"],
+		),
+	)
+	if err != nil {
+		logger.ErrorString("发送邮件", "发件出错", err.Error())
+		return false
+	}
+	logger.DebugString("发送邮件", "发件成功", "")
+	return true
+}
---
+++ pkg/mail/mail.go
@@ -0,0 +1,43 @@
+// Package mail 发送短信
+package mail
+
+import (
+	"gohub/pkg/config"
+	"sync"
+)
+
+type Form struct {
+	Address string
+	Name    string
+}
+
+type Email struct {
+	From    Form
+	To      []string
+	Bcc     []string
+	Cc      []string
+	Subject string
+	Text    []byte // Plaintext message (optional)
+	HTML    []byte // Html message (optional)
+}
+
+type Mailer struct {
+	Driver Driver
+}
+
+var once sync.Once
+var internalMailer *Mailer
+
+// NewMailer 单例模式获取
+func NewMailer() *Mailer {
+	once.Do(func() {
+		internalMailer = &Mailer{
+			Driver: &SMTP{},
+		}
+	})
+	return internalMailer
+}
+
+func (m *Mailer) Send(email Email) bool {
+	return m.Driver.Send(email, config.GetStringMapString("mail.smtp"))
+}
