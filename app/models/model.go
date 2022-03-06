bdbfde582c4ba32af4d8a68b9f685ce33992400e
gerry <13632429781@163.com>
2022-03-07 00:02:26 +0800

    feat(api): "JWT 授权

--- app/models/model.go
+++ app/models/model.go
@@ -1,7 +1,10 @@
 // Package models 模型通用属性和方法
 package models
 
-import "time"
+import (
+	"github.com/spf13/cast"
+	"time"
+)
 
 // BaseModel 模型基类
 type BaseModel struct {
@@ -12,4 +15,8 @@ type BaseModel struct {
 type CommonTimestampsField struct {
 	CreatedAt time.Time `gorm:"column:created_at;index" json:"created_at,omitempty"`
 	UpdatedAt time.Time `gorm:"column:updated_at;index" json:"updated_at,omitempty"`
+}
+
+func (b BaseModel) GetStringID() string {
+	return cast.ToString(b.ID)
 }
