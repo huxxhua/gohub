b0e83942bd09639cb03f01a6e315c49764578612
gerry <13632429781@163.com>
2022-03-15 10:22:51 +0800

    feat(api): console 包

--- go.mod
+++ go.mod
@@ -8,13 +8,13 @@ require (
 	github.com/go-redis/redis/v8 v8.11.4
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/jordan-wright/email v4.0.1-0.20210109023952-943e75fe5223+incompatible
+	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d
 	github.com/mojocn/base64Captcha v1.3.5
 	github.com/pborman/uuid v1.2.1 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/spf13/cast v1.4.1
 	github.com/spf13/viper v1.10.1
 	github.com/thedevsaddam/govalidator v1.9.10
-	github.com/ulule/limiter/v3 v3.9.0 // indirect
+	github.com/ulule/limiter/v3 v3.9.0
 	go.uber.org/zap v1.21.0
 	golang.org/x/crypto v0.0.0-20220307211146-efcb8507fb70
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
---
+++ pkg/console/console.go
@@ -0,0 +1,44 @@
+// Package console 命令行辅助方法
+package console
+
+import (
+	"fmt"
+	"github.com/mgutz/ansi"
+	"os"
+)
+
+// Success 打印一条成功消息，绿色输出
+func Success(msg string) {
+	colorOut(msg, "green")
+}
+
+// Error 打印一条报错消息，红色输出
+func Error(msg string) {
+	colorOut(msg, "red")
+}
+
+// Warning 打印一条提示消息，黄色输出
+func Warning(msg string) {
+	colorOut(msg, "yellow")
+}
+
+// Exit 打印一条报错消息，并退出 os.Exit(1)
+func Exit(msg string) {
+	Error(msg)
+	os.Exit(1)
+}
+
+// ExitIf 语法糖，自带 err != nil 判断
+func ExitIf(err error) {
+	if err != nil {
+		Error(err.Error())
+	}
+}
+
+// colorOut 内部使用，设置高亮颜色
+func colorOut(message, color string) {
+	_, err := fmt.Fprintln(os.Stdout, ansi.Color(message, color))
+	if err != nil {
+		panic(err)
+	}
+}
