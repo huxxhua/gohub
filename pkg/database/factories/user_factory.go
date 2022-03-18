f37b2c22197dc88062a62e57678c0e2d0c9df85b
gerry <13632429781@163.com>
2022-03-18 16:15:13 +0800

    feat(api): factory 和 Faker

---
+++ pkg/database/factories/user_factory.go
@@ -0,0 +1,28 @@
+// Package factories 存放工厂方法
+package factories
+
+import (
+	"github.com/bxcodec/faker/v3"
+	"gohub/app/models/user"
+	"gohub/pkg/helpers"
+)
+
+func MakeUsers(times int) []user.User {
+
+	var objs []user.User
+
+	// 设置唯一值
+	faker.SetGenerateUniqueValues(true)
+
+	for i := 0; i < times; i++ {
+		model := user.User{
+			Name:     faker.Name(),
+			Email:    faker.Email(),
+			Phone:    helpers.RandomNumber(11),
+			Password: "$2a$14$oPzVkIdwJ8KqY0erYAYQxOuAAlbI/sFIsH0C0R4MPc.3JbWWSuaUe",
+		}
+		objs = append(objs, model)
+	}
+
+	return objs
+}
