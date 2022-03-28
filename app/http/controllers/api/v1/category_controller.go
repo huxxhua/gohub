3121cdc5c51febcd96b32fb1d0d8c019fcb8c119
gerry <13632429781@163.com>
2022-03-28 15:18:31 +0800

    feat(api): 更新分类

--- app/http/controllers/api/v1/category_controller.go
+++ app/http/controllers/api/v1/category_controller.go
@@ -30,3 +30,30 @@ func (ctrl *CategoriesController) Store(c *gin.Context) {
 		response.Abort500(c, "创建失败，请稍后尝试~")
 	}
 }
+
+func (ctrl *CategoriesController) Update(ctx *gin.Context) {
+
+	// 验证 url 参数 id 是否正确
+	categoryModel := category.Get(ctx.Param("id"))
+	if categoryModel.ID == 0 {
+		response.Abort404(ctx)
+		return
+	}
+
+	// 表单验证
+	request := requests.CategoryRequest{}
+	if ok := requests.Validate(ctx, &request, requests.CategorySave); !ok {
+		return
+	}
+
+	// 保存数据
+	categoryModel.Name = request.Name
+	categoryModel.Description = request.Description
+	rowsAffected := categoryModel.Save()
+
+	if rowsAffected > 0 {
+		response.Data(ctx, categoryModel)
+	} else {
+		response.Abort404(ctx)
+	}
+}
