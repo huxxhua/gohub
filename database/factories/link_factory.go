fbe59b9b16880973df53803268200a700f849996
gerry <13632429781@163.com>
2022-03-28 16:36:33 +0800

    feat(api): 友情链接列表

---
+++ database/factories/link_factory.go
@@ -0,0 +1,21 @@
+package factories
+
+import (
+	"github.com/bxcodec/faker/v3"
+	"gohub/app/models/link"
+)
+
+func MakeLinks(count int) []link.Link {
+
+	var objs []link.Link
+
+	for i := 0; i < count; i++ {
+		linkModel := link.Link{
+			Name: faker.Username(),
+			URL:  faker.URL(),
+		}
+		objs = append(objs, linkModel)
+	}
+
+	return objs
+}
