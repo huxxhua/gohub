6a969d44252a81c85ccd5a65c61de42c51099bba
gerry <13632429781@163.com>
2022-03-29 14:11:05 +0800

    feat(api): cache forget 命令

---
+++ app/cmd/cache.go
@@ -0,0 +1,47 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"gohub/pkg/cache"
+	"gohub/pkg/console"
+)
+
+var CmdCache = &cobra.Command{
+	Use:   "cache",
+	Short: "Cache management",
+}
+
+var CmdCacheClear = &cobra.Command{
+	Use:   "clear",
+	Short: "Clear cache",
+	Run:   runCacheClear,
+}
+
+var CmdCacheForget = &cobra.Command{
+	Use:   "forget",
+	Short: "Delete redis key, example: cache forget cache-key",
+	Run:   runCacheForget,
+}
+
+// forget 命令的选项
+var cacheKey string
+
+func init() {
+	// 注册 cache 命令的子命令
+	CmdCache.AddCommand(CmdCacheClear, CmdCacheForget)
+
+	// 设置 cache forget 命令的选项
+	CmdCacheForget.Flags().StringVarP(&cacheKey, "key", "k", "", "KEY of the cache")
+	_ = CmdCacheForget.MarkFlagRequired("key")
+}
+
+func runCacheClear(cmd *cobra.Command, args []string) {
+	cache.Flush()
+	console.Success("Cache cleared.")
+}
+
+func runCacheForget(command *cobra.Command, args []string) {
+	cache.Forget(cacheKey)
+	console.Success(fmt.Sprintf("Cache key [%s] deleted.", cacheKey))
+}
