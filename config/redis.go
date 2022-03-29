ca77f531b9cb930772b17add4ba5292fdc9fd5da
gerry <13632429781@163.com>
2022-03-29 14:02:47 +0800

    feat(api): cache clear 命令

--- config/redis.go
+++ config/redis.go
@@ -11,11 +11,11 @@ func init() {
 			"username": config.Env("REDIS_USERNAME", ""),
 			"password": config.Env("REDIS_PASSWORD", ""),
 
-			// 业务类存储使用 1 (图片验证码、短信验证码、会话)
+			// 业务类存储使用 0 (图片验证码、短信验证码、会话)
 			"database": config.Env("REDIS_MAIN_DB", 0),
 
-			// 缓存 cache 包使用 0 ，缓存清空理应当不影响业务
-			"database_cache": config.Env("REDIS_CACHE_DB", 0),
+			// 缓存 cache 包使用 1 ，缓存清空理应当不影响业务
+			"database_cache": config.Env("REDIS_CACHE_DB", 1),
 		}
 	})
 }
